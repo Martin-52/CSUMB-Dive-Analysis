a97a7ba41b538ddd906a5556e89bc326d5aa935c
Jeremy Shaw <jjsjeremy61@>
2020-03-06 14:21:30 -0800

    added getRovNames

--- src/main/java/org/mbari/m3/dive/visualizer/DiveService.java
+++ src/main/java/org/mbari/m3/dive/visualizer/DiveService.java
@@ -4,15 +4,22 @@ import io.helidon.webserver.Routing;
 import io.helidon.webserver.ServerRequest;
 import io.helidon.webserver.ServerResponse;
 import io.helidon.webserver.Service;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import com.google.gson.Gson;
 
 import org.mbari.expd.Dive;
 import org.mbari.expd.DiveDAO;
 import org.mbari.expd.jdbc.BaseDAOImpl;
 import org.mbari.expd.jdbc.DiveDAOImpl;
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
 public class DiveService implements Service {
@@ -20,8 +27,8 @@ public class DiveService implements Service {
     @Override // this is called everytime this path is accessed
     public void update(Routing.Rules rules) {
         rules
-            .get("/{rov}", this::getRovDives)
-            .get("/getRovNames", this::getRovNames);
+            .get("/getRovNames", this::getRovNames)
+            .get("/{rov}", this::getRovDives);
     }
 
     /**
@@ -40,14 +47,20 @@ public class DiveService implements Service {
         response.send(json.toJSONString());
     }
     
+    /**
+     * Returns a list of ROV names.
+     * @param request
+     * @param response
+     */
     private void getRovNames(ServerRequest request, ServerResponse response) {
-        List<String> rovs = BaseDAOImpl.getAllRovNames();
-        String json = new Gson().toJson(rovs);
-        System.out.println(json);
+        JsonArray arr = new JsonArray();
+        for (String name: BaseDAOImpl.getAllRovNames()) {
+            arr.add(name);
+        }
         response.headers().add("Access-Control-Allow-Origin", "*");
         response.headers().add("Access-Control-Allow-Headers", "*");
         response.headers().add("Access-Control-Allow-Credentials", "true");
         response.headers().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
-        response.send(json);
+        response.send(arr.toString());
     }
 }
