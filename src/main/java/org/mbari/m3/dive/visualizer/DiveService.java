c1004518033410e512d8800fddd1868ba0983135
Antonio Vega <antoniovegajr@Antonios-MacBook-Pro.local>
2020-03-13 15:47:17 -0700

    removed dates from mins and depth list (and hour and depth list). Created function to get first dive captured date and last dive captured date. Cleaned and ordered imports too

--- src/main/java/org/mbari/m3/dive/visualizer/DiveService.java
+++ src/main/java/org/mbari/m3/dive/visualizer/DiveService.java
@@ -1,45 +1,25 @@
 package org.mbari.m3.dive.visualizer;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
 import io.helidon.webserver.Routing;
+import io.helidon.webserver.Service;
 import io.helidon.webserver.ServerRequest;
 import io.helidon.webserver.ServerResponse;
-import io.helidon.webserver.Service;
+import java.io.IOException;
+import java.sql.Date;
+import java.util.Calendar;
 import java.util.Collection;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.stream.Collectors;
 import org.mbari.expd.Dive;
 import org.mbari.expd.DiveDAO;
 import org.mbari.expd.jdbc.BaseDAOImpl;
 import org.mbari.expd.jdbc.DiveDAOImpl;
-import org.json.simple.JSONObject;
-
-import java.io.IOException;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.sql.Date;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.Calendar;
-import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
-
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import java.util.stream.Collectors;
-
-import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
-
-import org.mbari.expd.NavigationDatum;
-import org.mbari.expd.NavigationDatumDAO;
 import org.mbari.expd.jdbc.NavigationDatumDAOImpl;
-import org.mbari.expd.jdbc.NavigationDatumImpl;
-
+import org.mbari.expd.NavigationDatum;
 
 public class DiveService implements Service {
     private final Logger log = Logger.getLogger(getClass().getName());
@@ -76,6 +56,14 @@ public class DiveService implements Service {
         rules.get("/getHourAndDepth/{rov}/{diveNumber}", (req, res) -> {
             try {
                 getHourAndDepth(req, res);
+            } catch (IOException | InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        });
+        rules.get("/getDiveDates/{rov}/{diveNumber}", (req, res) -> {
+            try {
+                getDiveDates(req, res);
             } catch (IOException | InterruptedException e) {
                 // TODO Auto-generated catch block
                 e.printStackTrace();
@@ -159,21 +147,14 @@ public class DiveService implements Service {
 
         NavigationDatumDAOImpl dao1 = new NavigationDatumDAOImpl();
         List<NavigationDatum> nav = dao1.fetchBestNavigationData(dive);
-
         JsonArray minAndDepth = new JsonArray();
-        String date = "";
-
+        
         for(int i = 0 ; i < nav.size();i++){
             JsonObject newMinDepthObj = new JsonObject();
             Date dateObj = new Date(nav.get(i).getDate().getTime());
             Calendar calendar = Calendar.getInstance();    
             calendar.setTime(dateObj);
 
-            // CONCAT BETTER
-            String[] dateSplit = calendar.getTime().toString().split(" ");
-            date = dateSplit[0] + dateSplit[1] + dateSplit[2] + dateSplit[5];
-
-            newMinDepthObj.addProperty("date", date);
             newMinDepthObj.addProperty("minute", calendar.get(Calendar.MINUTE));
             newMinDepthObj.addProperty("depth", Double.toString(nav.get(i).getDepth()));
             minAndDepth.add(newMinDepthObj);
@@ -200,24 +181,14 @@ public class DiveService implements Service {
 
         NavigationDatumDAOImpl dao1 = new NavigationDatumDAOImpl();
         List<NavigationDatum> nav = dao1.fetchBestNavigationData(dive);
-
         JsonArray hourAndDepth = new JsonArray();
-        String date = "";
 
         for(int i = 0 ; i < nav.size();i++){
             JsonObject newHourDepthObj = new JsonObject();
-            
             Date dateObj = new Date(nav.get(i).getDate().getTime());
             Calendar calendar = Calendar.getInstance();    
             calendar.setTime(dateObj);
 
-        
-            // CONCAT BETTER
-            String[] dateSplit = calendar.getTime().toString().split(" ");
-            date = dateSplit[0] + dateSplit[1] + dateSplit[2] + dateSplit[5];
-            
-
-            newHourDepthObj.addProperty("date", date);
             newHourDepthObj.addProperty("hour", calendar.get(Calendar.HOUR_OF_DAY));
             newHourDepthObj.addProperty("depth", Double.toString(nav.get(i).getDepth()));
             hourAndDepth.add(newHourDepthObj);
@@ -245,5 +216,66 @@ public class DiveService implements Service {
         response.headers().add("Access-Control-Allow-Credentials", "true");
         response.headers().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
         response.send(arr.toString());
+    }
+
+
+    private void getDiveDates(ServerRequest request, ServerResponse response)throws IOException, InterruptedException {
+        String rovName = request.path().param("rov");
+        int diveNumber = Integer.parseInt(request.path().param("diveNumber"));
+        DiveDAO dao = new DiveDAOImpl();
+        Dive dive = dao.findByPlatformAndDiveNumber(rovName, diveNumber);
+
+        if(dive==null){
+            log.log(Level.WARNING, "getDiveDates(): null dive - DiveService");
+            return;
+        }
+
+        NavigationDatumDAOImpl dao1 = new NavigationDatumDAOImpl();
+        List<NavigationDatum> nav = dao1.fetchBestNavigationData(dive);
+
+        JsonObject dates = new JsonObject();
+        
+        if(nav.size()>0){
+            try {
+                Date startDateObj = new Date(nav.get(0).getDate().getTime());
+                Calendar calendar1 = Calendar.getInstance();    
+                calendar1.setTime(startDateObj);
+                StringBuilder startDate = new StringBuilder("");
+                String[] dateSplit1 = calendar1.getTime().toString().split(" ");
+                startDate.append(dateSplit1[0]);
+                startDate.append(", ");
+                startDate.append(dateSplit1[1]);
+                startDate.append(" ");
+                startDate.append(dateSplit1[2]);
+                startDate.append(", ");
+                startDate.append(dateSplit1[5]);
+                dates.addProperty("startDate", startDate.toString());
+    
+                Date endDateObj = new Date(nav.get(nav.size()-1).getDate().getTime());
+                Calendar calendar2 = Calendar.getInstance();    
+                calendar2.setTime(endDateObj);
+                StringBuilder endDate = new StringBuilder("");
+                String[] dateSplit2 = calendar2.getTime().toString().split(" ");
+                endDate.append(dateSplit2[0]);
+                endDate.append(", ");
+                endDate.append(dateSplit2[1]);
+                endDate.append(" ");
+                endDate.append(dateSplit2[2]);
+                endDate.append(", ");
+                endDate.append(dateSplit2[5]);
+                dates.addProperty("endDate", endDate.toString());
+
+            } catch(Exception e){
+                log.log(Level.WARNING, "getDiveDates(): unable to parse dates - DiveService");
+                e.printStackTrace();
+            }
+
+        }
+
+        response.headers().add("Access-Control-Allow-Origin", "*");
+        response.headers().add("Access-Control-Allow-Headers", "*");
+        response.headers().add("Access-Control-Allow-Credentials", "true");
+        response.headers().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+        response.send(dates.toString());
     }
 }
